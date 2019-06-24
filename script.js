ac632c078b3f7488e76cc1510118f3ca6b485b02
ovidiumilitaru <militaru.ovidiu@gmail.com>
2019-06-24 12:13:11 +0300

    what time is it?

---
+++ script.js
@@ -0,0 +1,24 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
+
+function runTheClock() {
+  hrPosition = hrPosition + ((360/12)/3600);
+  minPosition = minPosition + ((1/60)*6);
+  secPosition = secPosition + (360/60);
+
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
